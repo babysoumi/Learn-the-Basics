51f21014d408b8486e1bcdba313b7fb60e2b1f6f
babysoumi <104836458+babysoumi@users.noreply.github.com>
2024-09-24 18:44:13 +0530

    Add files via upload

--- Datatype.java
+++ Datatype.java
@@ -1,19 +1,30 @@
-class Datatype {
-    static int dataTypeSize(String str) {
-        // code here
-        switch(str) {
+public class Datatype {
+
+    public static int dataTypeSize(String dataType) {
+        switch (dataType) {
             case "Character":
                 return 2;
             case "Integer":
                 return 4;
             case "Long":
-                return 8;
+                return 8;  
             case "Float":
-                return 4;
+                return 4;  
             case "Double":
-                return 8;
+                return 8;  
             default:
-                return -1;
+                return -1;  
         }
     }
+
+    public static void main(String[] args) {
+        // Test cases
+        System.out.println(dataTypeSize("Character"));  
+        System.out.println(dataTypeSize("Integer"));    
+        System.out.println(dataTypeSize("Long"));       
+        System.out.println(dataTypeSize("Float"));      
+        System.out.println(dataTypeSize("Double"));     
+        System.out.println(dataTypeSize("String"));     
+    }
 }
+
