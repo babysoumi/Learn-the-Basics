a3f0bef276c316ad81dac9e662478754f730a965
babysoumi <104836458+babysoumi@users.noreply.github.com>
2024-10-02 18:56:57 +0530

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,12 @@
+public class Largest {
+    public static int largenum(int arr[])
+    {
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+                max=arr[i];
+        }
+        return max;
+    }
+}
