86819ed0d27fe7c11668e2272bae3e118fb4cdb8
babysoumi <104836458+babysoumi@users.noreply.github.com>
2024-10-08 15:18:34 +0530

    Add files via upload

---
+++ Movezerosend.java
@@ -0,0 +1,25 @@
+public class Movezerosend {
+    public static int moveend(int[] arr,int n)
+    {
+        int j=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {
+                j=i;
+                break;
+            }
+        }
+        for(int i=j+1;i<n;i++)
+        {
+            if(arr[i]!=0)
+            {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
+        return 0;
+    }
+}
