b4e4904c756bbcd4bf73d5b60d896e41211eb26e
babysoumi <104836458+babysoumi@users.noreply.github.com>
2024-10-10 21:25:31 +0530

    Add files via upload

---
+++ LongestsubarrayPN.java
@@ -0,0 +1,26 @@
+public class LongestsubarrayPN {
+    public static int positivenegative(int []a, int k) {
+        int n = a.length;
+
+        Map<Integer, Integer> preSumMap = new HashMap<>();
+        int sum = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            sum += a[i];
+            if (sum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+            int rem = sum - k;
+            if (preSumMap.containsKey(rem)) {
+                int len = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+            if (!preSumMap.containsKey(sum)) {
+                preSumMap.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+
+}
